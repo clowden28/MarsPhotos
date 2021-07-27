3737440fe8a6246d72d1c6aa47082fb1681d30e6
clowden28 <Jcpennies1!>
2021-07-27 19:05:59 -0400

    Pathway finsihed

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -1,3 +1,4 @@
+
 package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Json
@@ -8,6 +9,7 @@ import com.squareup.moshi.Json
  */
 data class MarsPhoto(
     val id: String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
     @Json(name = "img_src") val imgSrcUrl: String
 )
 
